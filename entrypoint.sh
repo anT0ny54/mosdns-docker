f36b9cbe4a8122ca86504171ac91029f540be9a1
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2022-09-01 01:19:36 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ if [ ! -f /etc/mosdns/config.yaml ]; then
 	cp -u /config.yaml /etc/mosdns/config.yaml
     	cp -u /geosite.dat /etc/mosdns/geosite.dat
     	cp -u /geoip.dat /etc/mosdns/geoip.dat
+    	cp -u /hosts /etc/mosdns/hosts
 fi
 
 sed -i "s|PORT_PLACEHOLDER|${PORT}|;s|PATH_PLACEHOLDER|${DOH_PATH}|" /etc/mosdns/config.yaml
