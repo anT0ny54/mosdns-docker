49a862408f1b825c70623a584bedfd58facd8379
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2022-09-20 03:46:18 +0700

    Update install_hosts.sh

--- install_hosts.sh
+++ install_hosts.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# Filename: update-hosts.sh
-# Author: George Lesica <george@lesica.com>
-# Description: Replaces the HOSTS file with a customized version that blocks
-# domains that serve ads and malicious software, creating a backup of the old
-# file.
+for (( ; ; ))
+do
+   echo "infinite loops hosts"
 
 HOSTS_URL="https://raw.githubusercontent.com/t0ny54/blocklistwithregex/main/export/blocklist.txt"
 NEW_HOSTS="hosts"
@@ -19,3 +17,7 @@ cp -v $NEW_HOSTS $HOSTS_PATH
 
 # Clean up old downloads
 rm $NEW_HOSTS*
+
+sleep 43200
+
+done
