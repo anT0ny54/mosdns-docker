26b72e397b4ea70dd53d0854894873ec6ecd9d36
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2022-09-20 05:28:58 +0700

    Update updatehosts.sh

--- updatehosts.sh
+++ updatehosts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 HOSTS_URL="https://raw.githubusercontent.com/t0ny54/blocklistwithregex/main/export/blocklist.txt"
-NEW_HOSTS="hosts"
+NEW_HOSTS="blocklist.txt"
 
 # Grab hosts file
 wget -O $NEW_HOSTS $HOSTS_URL
